06ef13b9e9e9b13cf7562a286164b24194a56a5b
IvanNikoleav <nik.ivan.mi@gmail.com>
2022-10-31 21:33:41 +0300

    create base template project + update html files and create base func

--- app/views.py
+++ app/views.py
@@ -21,6 +21,25 @@ def login():
     return render_template('accounts/login.html')
 
 
+@app.route('/new_product')
+def new_product():
+    return render_template('new_product.html')
+
+
+@app.route('/favourite')
+def favourite_user():
+    return render_template('favourite.html')
+
+
+@app.route('/cart')
+def cart_user():
+    return render_template('cart.html')
+
+
+@app.route('/profile')
+def profile_user():
+    return render_template('profile.html')
+
 @app.errorhandler(404)
 def pageNotFound(error):
     return render_template('page404.html', title='Страница не найдена !')
