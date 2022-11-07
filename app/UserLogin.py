0bc8bc0958e8addae7af39c708bd3002f92195c9
IvanNikoleav <nik.ivan.mi@gmail.com>
2022-11-07 12:32:57 +0300

    add login manager

---
+++ app/UserLogin.py
@@ -0,0 +1,21 @@
+from flask_login import UserMixin
+
+
+class UserLogin(UserMixin):
+    def __init__(self):
+        self.__user = None
+
+    def from_db(self, user_id, db):
+        self.__user = db.get_user(user_id)
+        return self
+
+    def create(self, user):
+        self.__user = user
+        return self
+
+    def get_id(self):
+        return str(self.__user[0]['id'])
+
+    def get_user(self, db, user_id):
+        self.__user = db.select(f"SELECT * FROM users WHERE id=%s LIMIT 1, (user_id,)")
+        return self
--- app/views.py
+++ app/views.py
@@ -1,67 +1,85 @@
 # -*- encoding: utf-8 -*-
 
 
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for, flash
 from jinja2 import TemplateNotFound
 from app import app
 from app import db_util
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import forms
 
-# from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-# from UserLogin import UserLogin
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from app.UserLogin import UserLogin
 
 db = db_util.Database()
+login_manager = LoginManager(app)
+app.debug = True
+
+login_manager.login_view = 'login'
+login_manager.login_message = 'Пожалуйста, авторизуйтесь для продолжения'
+login_manager.login_message_category = 'success'
 
 
-# login_manager = LoginManager(app)
+@login_manager.user_loader
+def load_user(user_id):
+    print('load user')
+    return UserLogin().from_db(user_id, db)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Главная')
+    is_auth = current_user.is_authenticated
+    print(is_auth)
+    return render_template('index.html', is_auth=is_auth)
 
 
 @app.route('/registration', methods=['POST', 'GET'])
 def register():
     form = forms.RegistrForm()
-    result = None
-    error = None
-    if request.method == 'POST':
-        username = request.form.get('username'),
-        email = request.form.get('email')
-        password = request.form.get('password')
-        print(username)
-        result = db.create_user(username[0], email, generate_password_hash(password))
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        username = form.username.data
+        print(type(username))
+        result = db.create_user(username, email, generate_password_hash(password))
         if result is False:
             status = 'Пользователь с Email уже существует !'
             return render_template('accounts/register.html', form=form, status=status)
         else:
             if result is True:
                 status = 'Вы успешно зарегистрировались !'
-                return render_template('accounts/login.html', form=form, status=status)
+                return redirect(url_for('login'))
     return render_template('accounts/register.html', form=form)
 
 
 @app.route('/authorization', methods=['POST', 'GET'])
 def login():
     form = forms.AuthForm()
-    result = False
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-        result = db.is_authenticated(email, password)
-        if result is False:
-            status = 'Вы ввели неверные данные !'
-            return render_template('accounts/login.html', form=form, status=status)
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        remember = form.remember.data
+        user = db.get_user_by_email(email)
+        print(user)
+        if user:
+            if check_password_hash(user[0]['password'], password):
+                user_login = UserLogin().create(user)
+                rm = remember
+                login_user(user_login, remember=rm)
+                return redirect(url_for('index'))
+            else:
+                status = 'Вы ввели неверные данные !'
+                return render_template('accounts/login.html', form=form, status=status)
         else:
-            if result is True:
-                status = None
-                return render_template('index.html', form=form, status=status)
+            status = 'Вы не зарегистрировались !'
+            return render_template('accounts/login.html', form=form, status=status)
+    else:
+        pass
     return render_template('accounts/login.html', form=form)
 
 
 @app.route('/new_product')
+@login_required
 def new_product():
     return render_template('new_product.html')
 
