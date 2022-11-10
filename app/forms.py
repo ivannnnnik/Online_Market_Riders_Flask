45bc6c02a9353a7db2720cf1dc7a80f0960f5fa3
IvanNikoleav <nik.ivan.mi@gmail.com>
2022-11-10 14:26:22 +0300

    add CreateProduct form

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,13 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, PasswordField
 from wtforms.validators import DataRequired, Email, Length
 import email_validator
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from werkzeug.utils import secure_filename
+
+
+# class SearchProducts(FlaskForm):
+#     name_product = StringField(validators=[DataRequired()])
+#     submit = SubmitField("Найти")
 
 class RegistrForm(FlaskForm):
     username = StringField("Имя: ", validators=[DataRequired()])
@@ -9,7 +16,24 @@ class RegistrForm(FlaskForm):
     password = PasswordField("Пароль: ", validators=[DataRequired(), Length(min=8, max=25)])
     submit = SubmitField("Отправить")
 
+
 class AuthForm(FlaskForm):
-    email = StringField("Email: ", validators=[Email()])
-    password = PasswordField("Пароль: ", validators=[DataRequired(), Length(min=8, max=25)])
+    email = StringField("Email: ", validators=[Email('Некорректный Email')])
+    password = PasswordField("Пароль: ", validators=[DataRequired(),
+                                                     Length(min=8,
+                                                            max=25,
+                                                            message='Пароль должен быть от 8 до 25 символов')
+                                                     ]
+                             )
+    remember = BooleanField('Запомнить меня: ')
+    submit = SubmitField("Отправить")
+
+
+class CreateProduct(FlaskForm):
+    name = StringField("Название: ", validators=[DataRequired()])
+    text = TextAreaField("Описание: ", validators=[DataRequired()])
+    price = IntegerField('Цена: ', validators=[DataRequired()])
+    photo = FileField('Картинка: ', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg'])])
+    count = IntegerField('Количество: ', validators=[DataRequired()])
+    type_product = StringField("Тип товара: ", validators=[DataRequired()])
     submit = SubmitField("Отправить")
